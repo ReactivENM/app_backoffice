848a39ccfa713d55851d7e552b4082bf3c53463b
ReactivENM <reactivenm2023@gmail.com>
2023-10-03 22:24:58 -0300

    Creacion de la ventana de almacenes

---
+++ WindowsFormsApp1/Forms/FormWarehouse.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApp1.Forms
+{
+    partial class FormWarehouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWarehouse));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDeleteWarehouse = new System.Windows.Forms.Button();
+            this.btnEditWarehouse = new System.Windows.Forms.Button();
+            this.btnCreateWarehouse = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblPage = new System.Windows.Forms.Label();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.calle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nro_puerta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cod_postal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capacidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridView);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(868, 489);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.calle,
+            this.nro_puerta,
+            this.cod_postal,
+            this.capacidad});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.EnableHeadersVisualStyles = false;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(868, 489);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.btnDeleteWarehouse);
+            this.panel2.Controls.Add(this.btnEditWarehouse);
+            this.panel2.Controls.Add(this.btnCreateWarehouse);
+            this.panel2.Controls.Add(this.btnNext);
+            this.panel2.Controls.Add(this.lblPage);
+            this.panel2.Controls.Add(this.btnPrev);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 507);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(892, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnDeleteWarehouse
+            // 
+            this.btnDeleteWarehouse.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnDeleteWarehouse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDeleteWarehouse.BackgroundImage")));
+            this.btnDeleteWarehouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDeleteWarehouse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDeleteWarehouse.Enabled = false;
+            this.btnDeleteWarehouse.ImageKey = "(none)";
+            this.btnDeleteWarehouse.Location = new System.Drawing.Point(94, 13);
+            this.btnDeleteWarehouse.Name = "btnDeleteWarehouse";
+            this.btnDeleteWarehouse.Size = new System.Drawing.Size(35, 35);
+            this.btnDeleteWarehouse.TabIndex = 5;
+            this.btnDeleteWarehouse.Text = "\r\n";
+            this.btnDeleteWarehouse.UseVisualStyleBackColor = true;
+            this.btnDeleteWarehouse.Click += new System.EventHandler(this.btnDeleteWarehouse_Click);
+            // 
+            // btnEditWarehouse
+            // 
+            this.btnEditWarehouse.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnEditWarehouse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEditWarehouse.BackgroundImage")));
+            this.btnEditWarehouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEditWarehouse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditWarehouse.Enabled = false;
+            this.btnEditWarehouse.Location = new System.Drawing.Point(53, 13);
+            this.btnEditWarehouse.Name = "btnEditWarehouse";
+            this.btnEditWarehouse.Size = new System.Drawing.Size(35, 35);
+            this.btnEditWarehouse.TabIndex = 4;
+            this.btnEditWarehouse.UseVisualStyleBackColor = true;
+            this.btnEditWarehouse.Click += new System.EventHandler(this.btnEditWarehouse_Click);
+            // 
+            // btnCreateWarehouse
+            // 
+            this.btnCreateWarehouse.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnCreateWarehouse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCreateWarehouse.BackgroundImage")));
+            this.btnCreateWarehouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCreateWarehouse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCreateWarehouse.Location = new System.Drawing.Point(12, 13);
+            this.btnCreateWarehouse.Name = "btnCreateWarehouse";
+            this.btnCreateWarehouse.Size = new System.Drawing.Size(35, 35);
+            this.btnCreateWarehouse.TabIndex = 3;
+            this.btnCreateWarehouse.UseVisualStyleBackColor = true;
+            this.btnCreateWarehouse.Click += new System.EventHandler(this.btnCreateWarehouse_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNext.Location = new System.Drawing.Point(481, 15);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 33);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Siguiente";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblPage
+            // 
+            this.lblPage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPage.Location = new System.Drawing.Point(425, 25);
+            this.lblPage.Name = "lblPage";
+            this.lblPage.Size = new System.Drawing.Size(50, 13);
+            this.lblPage.TabIndex = 1;
+            this.lblPage.Text = "1/1";
+            this.lblPage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPrev.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrev.Location = new System.Drawing.Point(353, 15);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(66, 33);
+            this.btnPrev.TabIndex = 0;
+            this.btnPrev.Text = "Anterior";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // calle
+            // 
+            this.calle.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.calle.HeaderText = "Calle";
+            this.calle.Name = "calle";
+            this.calle.ReadOnly = true;
+            // 
+            // nro_puerta
+            // 
+            this.nro_puerta.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nro_puerta.HeaderText = "Número de puerta";
+            this.nro_puerta.Name = "nro_puerta";
+            this.nro_puerta.ReadOnly = true;
+            // 
+            // cod_postal
+            // 
+            this.cod_postal.HeaderText = "Código postal";
+            this.cod_postal.Name = "cod_postal";
+            this.cod_postal.ReadOnly = true;
+            // 
+            // capacidad
+            // 
+            this.capacidad.HeaderText = "Capacidad";
+            this.capacidad.Name = "capacidad";
+            this.capacidad.ReadOnly = true;
+            // 
+            // FormWarehouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 567);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormWarehouse";
+            this.Text = "Almacen";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblPage;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnEditWarehouse;
+        private System.Windows.Forms.Button btnCreateWarehouse;
+        private System.Windows.Forms.Button btnDeleteWarehouse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn calle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nro_puerta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod_postal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn capacidad;
+    }
+}
---
+++ WindowsFormsApp1/Forms/FormWarehouse.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using WindowsFormsApp1.Models;
+using WindowsFormsApp1.Controllers.Warehouse;
+
+namespace WindowsFormsApp1.Forms
+{
+    public interface HandleWarehouse
+    {
+        void OnCreateWarehouse(int id, string calle, string nro_puerta, string cod_postal, string capacidad);
+        void OnEditWarehouse(int id, string calle, string nro_puerta, string cod_postal, string capacidad);
+    }
+
+    public partial class FormWarehouse : Form, HandleWarehouse
+    {
+        List<WareHouse> wareHouseData = new List<WareHouse>();
+        private int wareHouseLength = 0;
+        private const int rowsPerPage = 10;
+
+        private int lastPage = 1;
+        private int actualPage = 1;
+
+        private bool isRowSelected = false;
+        WareHouse selectedWarehouse = null;
+
+        public FormWarehouse()
+        {
+            initializeFormAsync();
+        }
+
+        public async void initializeFormAsync()
+        {
+            InitializeComponent();
+
+            await fetchWareHouseData();
+
+            int lastPageRes = (int)Math.Ceiling((double)wareHouseLength / rowsPerPage);
+            lastPage = Convert.ToInt32(lastPageRes);
+            lblPage.Text = actualPage.ToString() + "/" + lastPage.ToString();
+
+            // Load mock data on dataGrid
+            showRows(actualPage);
+            // On double click dataGrid row
+            dataGridView.CellClick += new DataGridViewCellEventHandler(handleClickRow);
+
+            // Update buttons image when disabled/enabled
+            UpdateButtonImage(btnEditWarehouse, "../../Icons/edit.png", "../../Icons/disabled/edit.png");
+            UpdateButtonImage(btnDeleteWarehouse, "../../Icons/delete.png", "../../Icons/disabled/delete.png");
+        }
+
+        public async Task fetchWareHouseData()
+        {
+            WareHouseController wh = new WareHouseController();
+            List<WareHouse> warehouses = wh.GetAll();
+            foreach (WareHouse warehouse in warehouses)
+            {
+                wareHouseData.Add(warehouse);
+            }
+            wareHouseLength = warehouses.Count;
+        }
+
+        private void showRows(int page)
+        {
+            dataGridView.Rows.Clear();
+            for (int i = (page - 1) * rowsPerPage; i < (page * rowsPerPage > wareHouseLength ? wareHouseLength : page * rowsPerPage); i++)
+            {
+                DataGridViewRow newRow = new DataGridViewRow();
+                newRow.CreateCells(dataGridView, wareHouseData[i].id, wareHouseData[i].calle, wareHouseData[i].nro_puerta, wareHouseData[i].cod_postal, wareHouseData[i].capacidad);
+                dataGridView.Rows.Add(newRow);
+            }
+        }
+
+        private void handleClickRow(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                isRowSelected = true;
+
+                DataGridViewRow row = dataGridView.Rows[e.RowIndex];
+                object id = row.Cells["id"].Value;
+                object calle = row.Cells["calle"].Value;
+                object nro_puerta = row.Cells["nro_puerta"].Value;
+                object cod_postal = row.Cells["cod_postal"].Value;
+                object capacidad = row.Cells["capacidad"].Value;
+                WareHouse wh = new WareHouse(Convert.ToInt32(id), calle.ToString(), nro_puerta.ToString(), cod_postal.ToString(), capacidad.ToString());
+                selectedWarehouse = wh;
+                btnEditWarehouse.Enabled = true;
+                btnDeleteWarehouse.Enabled = true;
+            }
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (wareHouseLength == 0) return;
+            if (actualPage == 1) return;
+
+            actualPage = actualPage - 1;
+            lblPage.Text = actualPage.ToString() + "/" + lastPage;
+            showRows(actualPage);
+
+            // Disable buttons and unselect actual warehouse
+            selectedWarehouse = null;
+            btnEditWarehouse.Enabled = false;
+            btnDeleteWarehouse.Enabled = false;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (wareHouseLength == 0) return;
+            if (actualPage == lastPage) return;
+
+            actualPage = actualPage + 1;
+            lblPage.Text = actualPage.ToString() + "/" + lastPage;
+            showRows(actualPage);
+
+            // Disable buttons and unselect actual warehouse
+            selectedWarehouse = null;
+            btnEditWarehouse.Enabled = false;
+            btnDeleteWarehouse.Enabled = false;
+        }
+
+        private void btnCreateWarehouse_Click(object sender, EventArgs e)
+        {
+            new Forms.Warehouse.CreateWarehouse(this).ShowDialog();
+        }
+
+        public void OnCreateWarehouse(int id, string calle, string nro_puerta, string cod_postal, string capacidad)
+        {
+            WareHouse wh = new WareHouse(id, calle, nro_puerta, cod_postal, capacidad);
+            wareHouseData.Add(wh);
+            wareHouseLength += 1;
+            int lastPageRes = (int)Math.Ceiling((double)wareHouseLength / rowsPerPage);
+            lastPage = Convert.ToInt32(lastPageRes);
+            lblPage.Text = actualPage.ToString() + "/" + lastPage;
+            showRows(actualPage);
+
+            // Disable buttons and unselect actual warehouse
+            selectedWarehouse = null;
+            btnEditWarehouse.Enabled = false;
+            btnDeleteWarehouse.Enabled = false;
+        }
+
+        private void btnEditWarehouse_Click(object sender, EventArgs e)
+        {
+            Form warehouse = new Forms.Warehouse.EditWarehouse(selectedWarehouse.id, selectedWarehouse.calle, selectedWarehouse.nro_puerta, selectedWarehouse.cod_postal, selectedWarehouse.capacidad, this);
+            warehouse.ShowDialog();
+        }
+
+        public void OnEditWarehouse(int id, string calle, string nro_puerta, string cod_postal, string capacidad)
+        {
+            WareHouse editedWarehouse = wareHouseData.Find(wh => wh.id == id);
+            if (editedWarehouse == null) return;
+            editedWarehouse.calle = calle;
+            editedWarehouse.nro_puerta = nro_puerta;
+            editedWarehouse.cod_postal = cod_postal;
+            editedWarehouse.capacidad = capacidad;
+            showRows(actualPage);
+
+            // Disable buttons and unselect actual warehouse
+            selectedWarehouse = null;
+            btnEditWarehouse.Enabled = false;
+            btnDeleteWarehouse.Enabled = false;
+        }
+
+        private async void btnDeleteWarehouse_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                WareHouseController controller = new WareHouseController();
+                bool res = controller.Delete(selectedWarehouse.id);
+
+                if (res == true)
+                {
+                    wareHouseData.RemoveAll(wh => wh.id == selectedWarehouse.id);
+                    // Update DataGrid Table
+                    wareHouseLength = wareHouseData.Count;
+                    int lastPageRes = (int)Math.Ceiling((double)wareHouseData.Count / rowsPerPage);
+                    lastPage = Convert.ToInt32(lastPageRes);
+                    if ((actualPage - 1) * rowsPerPage == wareHouseLength)
+                    {
+                        if (wareHouseLength == 0) return;
+                        if (actualPage == 1) return;
+
+                        actualPage = actualPage - 1;
+                        lblPage.Text = actualPage.ToString() + "/" + lastPage;
+                        showRows(actualPage);
+
+                        // Disable buttons and unselect actual warehouse
+                        selectedWarehouse = null;
+                        btnEditWarehouse.Enabled = false;
+                        btnDeleteWarehouse.Enabled = false;
+                        return;
+                    }
+                    showRows(actualPage);
+
+                    // Disable buttons and unselect actual warehouse
+                    selectedWarehouse = null;
+                    btnEditWarehouse.Enabled = false;
+                    btnDeleteWarehouse.Enabled = false;
+                }
+                else MessageBox.Show("Hubo un error. Refresca la lista de almacenes.");
+            }
+        }
+
+        private void UpdateButtonImage(Button btn, string enabled, string disabled)
+        {
+            Image enabledImage = Image.FromFile(enabled);
+            Image disabledImage = Image.FromFile(disabled);
+
+            btn.EnabledChanged += (sender, e) =>
+            {
+                btn.BackgroundImage = btn.Enabled ? enabledImage : disabledImage;
+            };
+        }
+    }
+}
