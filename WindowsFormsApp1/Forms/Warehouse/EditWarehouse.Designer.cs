3e87bdd2dbba566702f627e3ce168055dedcf7ea
ReactivENM <reactivenm2023@gmail.com>
2023-10-03 22:32:52 -0300

    Formulario para editar almacen

---
+++ WindowsFormsApp1/Forms/Warehouse/EditWarehouse.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp1.Forms.Warehouse
+{
+    partial class EditWarehouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.inputNumber = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.inputStreet = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.labelPostalCode = new System.Windows.Forms.Label();
+            this.InputPostalCode = new System.Windows.Forms.TextBox();
+            this.labelCapacity = new System.Windows.Forms.Label();
+            this.inputCapacity = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(12, 71);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(345, 308);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel3.Controls.Add(this.labelCapacity);
+            this.panel3.Controls.Add(this.inputCapacity);
+            this.panel3.Controls.Add(this.labelPostalCode);
+            this.panel3.Controls.Add(this.InputPostalCode);
+            this.panel3.Controls.Add(this.btnSubmit);
+            this.panel3.Controls.Add(this.lblNumber);
+            this.panel3.Controls.Add(this.inputNumber);
+            this.panel3.Controls.Add(this.lblName);
+            this.panel3.Controls.Add(this.inputStreet);
+            this.panel3.Location = new System.Drawing.Point(24, 30);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(294, 249);
+            this.panel3.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(3, 208);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(288, 38);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Editar almacen";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(3, 48);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(92, 13);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "Número de puerta";
+            // 
+            // inputNumber
+            // 
+            this.inputNumber.Location = new System.Drawing.Point(3, 64);
+            this.inputNumber.Name = "inputNumber";
+            this.inputNumber.Size = new System.Drawing.Size(288, 20);
+            this.inputNumber.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(3, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(30, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Calle";
+            // 
+            // inputStreet
+            // 
+            this.inputStreet.Location = new System.Drawing.Point(3, 16);
+            this.inputStreet.Name = "inputStreet";
+            this.inputStreet.Size = new System.Drawing.Size(288, 20);
+            this.inputStreet.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(369, 65);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(369, 65);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Editar almacen";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelPostalCode
+            // 
+            this.labelPostalCode.AutoSize = true;
+            this.labelPostalCode.Location = new System.Drawing.Point(3, 96);
+            this.labelPostalCode.Name = "labelPostalCode";
+            this.labelPostalCode.Size = new System.Drawing.Size(71, 13);
+            this.labelPostalCode.TabIndex = 6;
+            this.labelPostalCode.Text = "Código postal";
+            // 
+            // InputPostalCode
+            // 
+            this.InputPostalCode.Location = new System.Drawing.Point(3, 112);
+            this.InputPostalCode.Name = "InputPostalCode";
+            this.InputPostalCode.Size = new System.Drawing.Size(288, 20);
+            this.InputPostalCode.TabIndex = 5;
+            // 
+            // labelCapacity
+            // 
+            this.labelCapacity.AutoSize = true;
+            this.labelCapacity.Location = new System.Drawing.Point(3, 146);
+            this.labelCapacity.Name = "labelCapacity";
+            this.labelCapacity.Size = new System.Drawing.Size(58, 13);
+            this.labelCapacity.TabIndex = 8;
+            this.labelCapacity.Text = "Capacidad";
+            // 
+            // inputCapacity
+            // 
+            this.inputCapacity.Location = new System.Drawing.Point(3, 162);
+            this.inputCapacity.Name = "inputCapacity";
+            this.inputCapacity.Size = new System.Drawing.Size(288, 20);
+            this.inputCapacity.TabIndex = 7;
+            // 
+            // EditWarehouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 391);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(385, 430);
+            this.MinimumSize = new System.Drawing.Size(385, 430);
+            this.Name = "EditWarehouse";
+            this.Text = "EditWarehouse";
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox inputNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox inputStreet;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label labelPostalCode;
+        private System.Windows.Forms.TextBox InputPostalCode;
+        private System.Windows.Forms.Label labelCapacity;
+        private System.Windows.Forms.TextBox inputCapacity;
+    }
+}
