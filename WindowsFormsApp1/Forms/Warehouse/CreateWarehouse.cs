659cb9391237d6df89fe6f9c51e5d8c4f8cef482
ReactivENM <146779481+ReactivENM@users.noreply.github.com>
2023-10-03 22:35:06 -0300

    Funcionalidad almacen (#1)
    * Creacion de la ventana de almacenes
    
    * Creacion de la ventana de login
    
    * Creacion de la ventana de usuarios
    
    * Creacion de la ventana de paquetes
    
    * Creacion de la ventana de lotes
    
    * Creacion de la ventana de camiones
    
    * Creacion de la ventana de rutas
    
    * Carpeta con imagenes (crear, editar y eliminar)
    
    * Model de Almacen
    
    * Mock data de almacenes
    
    * Conexion a la base de datos
    
    * Controller de almacenes
    
    * Formulario para crear almacen
    
    * Formulario para editar almacen

---
+++ WindowsFormsApp1/Forms/Warehouse/CreateWarehouse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormsApp1.Controllers.Warehouse;
+
+namespace WindowsFormsApp1.Forms.Warehouse
+{
+    public partial class CreateWarehouse : Form
+    {
+        private HandleWarehouse callback;
+
+        public CreateWarehouse(HandleWarehouse callback)
+        {
+            InitializeComponent();
+            this.callback = callback;
+        }
+
+        private async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool fieldsValid = validateFields();
+            if (!fieldsValid) return;
+
+            string calle = inputAddress.Text;
+            string nro_puerta = inputNumber.Text;
+            string cod_postal = inputPostalCode.Text;
+            string capacidad = inputCapacity.Text;
+
+            WareHouseController controller = new WareHouseController();
+            int id = controller.Create(calle, nro_puerta, cod_postal, int.Parse(capacidad));
+            callback.OnCreateWarehouse(id, calle, nro_puerta, cod_postal, capacidad.ToString());
+
+            this.Close();
+        }
+
+        private bool validateFields()
+        {
+            string name = inputAddress.Text;
+            string address = inputNumber.Text;
+            if(name.Length == 0 || address.Length == 0)
+            {
+                MessageBox.Show("Debes llenar todos los campos!", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Forms/Warehouse/EditWarehouse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormsApp1.Controllers.Warehouse;
+
+namespace WindowsFormsApp1.Forms.Warehouse
+{
+    public partial class EditWarehouse : Form
+    {
+        private HandleWarehouse callback;
+        private int id;
+
+        public EditWarehouse(int id, string calle, string nro_puerta, string cod_postal, string capacidad, HandleWarehouse callback)
+        {
+            InitializeComponent();
+            this.callback = callback;
+            this.id = id;
+            inputStreet.Text = calle;
+            inputNumber.Text = nro_puerta;
+            InputPostalCode.Text = cod_postal;
+            inputCapacity.Text = capacidad;
+        }
+
+        private async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool fieldsValid = validateFields();
+            if (!fieldsValid) return;
+
+            string calle = inputStreet.Text;
+            string nro_puerta = inputNumber.Text;
+            string cod_postal = InputPostalCode.Text;
+            int capacidad = int.Parse(inputCapacity.Text);
+
+            WareHouseController controller = new WareHouseController();
+            controller.Edit(id, calle, nro_puerta, cod_postal, capacidad);
+            callback.OnEditWarehouse(id, calle, nro_puerta, cod_postal, capacidad.ToString());
+
+            this.Close();
+        }
+
+        private bool validateFields()
+        {
+            string calle = inputStreet.Text;
+            string nro_puerta = inputNumber.Text;
+            string cod_postal = InputPostalCode.Text;
+            string capacidad = inputCapacity.Text;
+            if (calle.Length == 0 || nro_puerta.Length == 0 || cod_postal.Length == 0 || capacidad.Length == 0)
+            {
+                MessageBox.Show("Debes llenar todos los campos!", "Error de validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+    }
+}
