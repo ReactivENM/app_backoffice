55fd4c204af5b009679bccb255167e453c40d50e
ReactivENM <reactivenm2023@gmail.com>
2023-10-03 22:21:47 -0300

    Inicializacion del backoffice y los paquetes necesarios

---
+++ WindowsFormsApp1/Backoffice.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Backoffice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnTrayectos = new System.Windows.Forms.Button();
+            this.btnPaquetes = new System.Windows.Forms.Button();
+            this.btnLotes = new System.Windows.Forms.Button();
+            this.btnCamiones = new System.Windows.Forms.Button();
+            this.btnAlmacen = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.panelName = new System.Windows.Forms.Panel();
+            this.labelName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panelName.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelMenu.Controls.Add(this.btnTrayectos);
+            this.panelMenu.Controls.Add(this.btnPaquetes);
+            this.panelMenu.Controls.Add(this.btnLotes);
+            this.panelMenu.Controls.Add(this.btnCamiones);
+            this.panelMenu.Controls.Add(this.btnAlmacen);
+            this.panelMenu.Controls.Add(this.btnUsuarios);
+            this.panelMenu.Controls.Add(this.panelName);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(230, 561);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnTrayectos
+            // 
+            this.btnTrayectos.BackColor = System.Drawing.Color.Transparent;
+            this.btnTrayectos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTrayectos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTrayectos.FlatAppearance.BorderSize = 0;
+            this.btnTrayectos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrayectos.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrayectos.Location = new System.Drawing.Point(0, 260);
+            this.btnTrayectos.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTrayectos.Name = "btnTrayectos";
+            this.btnTrayectos.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnTrayectos.Size = new System.Drawing.Size(230, 40);
+            this.btnTrayectos.TabIndex = 7;
+            this.btnTrayectos.Text = "Trayectos";
+            this.btnTrayectos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrayectos.UseVisualStyleBackColor = false;
+            this.btnTrayectos.Click += new System.EventHandler(this.btnTrayectos_Click);
+            // 
+            // btnPaquetes
+            // 
+            this.btnPaquetes.BackColor = System.Drawing.Color.Transparent;
+            this.btnPaquetes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPaquetes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPaquetes.FlatAppearance.BorderSize = 0;
+            this.btnPaquetes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPaquetes.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaquetes.Location = new System.Drawing.Point(0, 220);
+            this.btnPaquetes.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPaquetes.Name = "btnPaquetes";
+            this.btnPaquetes.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnPaquetes.Size = new System.Drawing.Size(230, 40);
+            this.btnPaquetes.TabIndex = 6;
+            this.btnPaquetes.Text = "Paquetes";
+            this.btnPaquetes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPaquetes.UseVisualStyleBackColor = false;
+            this.btnPaquetes.Click += new System.EventHandler(this.btnPaquetes_Click);
+            // 
+            // btnLotes
+            // 
+            this.btnLotes.BackColor = System.Drawing.Color.Transparent;
+            this.btnLotes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLotes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLotes.FlatAppearance.BorderSize = 0;
+            this.btnLotes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLotes.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLotes.Location = new System.Drawing.Point(0, 180);
+            this.btnLotes.Margin = new System.Windows.Forms.Padding(0);
+            this.btnLotes.Name = "btnLotes";
+            this.btnLotes.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnLotes.Size = new System.Drawing.Size(230, 40);
+            this.btnLotes.TabIndex = 5;
+            this.btnLotes.Text = "Lotes";
+            this.btnLotes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLotes.UseVisualStyleBackColor = false;
+            this.btnLotes.Click += new System.EventHandler(this.btnLotes_Click);
+            // 
+            // btnCamiones
+            // 
+            this.btnCamiones.BackColor = System.Drawing.Color.Transparent;
+            this.btnCamiones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCamiones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCamiones.FlatAppearance.BorderSize = 0;
+            this.btnCamiones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCamiones.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCamiones.Location = new System.Drawing.Point(0, 140);
+            this.btnCamiones.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCamiones.Name = "btnCamiones";
+            this.btnCamiones.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnCamiones.Size = new System.Drawing.Size(230, 40);
+            this.btnCamiones.TabIndex = 4;
+            this.btnCamiones.Text = "Camiones";
+            this.btnCamiones.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCamiones.UseVisualStyleBackColor = false;
+            this.btnCamiones.Click += new System.EventHandler(this.btnCamiones_Click);
+            // 
+            // btnAlmacen
+            // 
+            this.btnAlmacen.BackColor = System.Drawing.Color.Transparent;
+            this.btnAlmacen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAlmacen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAlmacen.FlatAppearance.BorderSize = 0;
+            this.btnAlmacen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlmacen.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlmacen.Location = new System.Drawing.Point(0, 100);
+            this.btnAlmacen.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAlmacen.Name = "btnAlmacen";
+            this.btnAlmacen.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnAlmacen.Size = new System.Drawing.Size(230, 40);
+            this.btnAlmacen.TabIndex = 3;
+            this.btnAlmacen.Text = "Almacen";
+            this.btnAlmacen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAlmacen.UseVisualStyleBackColor = false;
+            this.btnAlmacen.Click += new System.EventHandler(this.btnAlmacen_Click);
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.BackColor = System.Drawing.Color.Transparent;
+            this.btnUsuarios.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUsuarios.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUsuarios.FlatAppearance.BorderSize = 0;
+            this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuarios.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Location = new System.Drawing.Point(0, 60);
+            this.btnUsuarios.Margin = new System.Windows.Forms.Padding(0);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnUsuarios.Size = new System.Drawing.Size(230, 40);
+            this.btnUsuarios.TabIndex = 2;
+            this.btnUsuarios.Text = "Usuarios";
+            this.btnUsuarios.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuarios.UseVisualStyleBackColor = false;
+            this.btnUsuarios.Click += new System.EventHandler(this.btnUsuarios_Click);
+            // 
+            // panelName
+            // 
+            this.panelName.Controls.Add(this.labelName);
+            this.panelName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelName.Location = new System.Drawing.Point(0, 0);
+            this.panelName.Margin = new System.Windows.Forms.Padding(0);
+            this.panelName.Name = "panelName";
+            this.panelName.Size = new System.Drawing.Size(230, 60);
+            this.panelName.TabIndex = 0;
+            // 
+            // labelName
+            // 
+            this.labelName.BackColor = System.Drawing.Color.White;
+            this.labelName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelName.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(0, 0);
+            this.labelName.Margin = new System.Windows.Forms.Padding(3, 0, 3, 5);
+            this.labelName.Name = "labelName";
+            this.labelName.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.labelName.Size = new System.Drawing.Size(230, 60);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Quick Carry";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(230, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(852, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.White;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.lblTitle.Size = new System.Drawing.Size(852, 60);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Inicio";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1082, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelMenu);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Form1";
+            this.Text = "Quick Carry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelName.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Panel panelName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button btnAlmacen;
+        private System.Windows.Forms.Button btnCamiones;
+        private System.Windows.Forms.Button btnTrayectos;
+        private System.Windows.Forms.Button btnPaquetes;
+        private System.Windows.Forms.Button btnLotes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ WindowsFormsApp1/Backoffice.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.DB;
+
+namespace WindowsFormsApp1
+{
+    public partial class Backoffice : Form
+    {
+        // Fields
+        private Form activeForm;
+
+        public Backoffice()
+        {
+            InitializeComponent();
+            this.FormClosing += onCloseBackoffice;
+        }
+
+        private void onCloseBackoffice(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormWarehouse(), sender);
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            Controls.Add(childForm);
+            Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitle.Text = childForm.Text;
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormUser(), sender);
+        }
+
+        private void btnAlmacen_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormWarehouse(), sender);
+        }
+
+        private void btnCamiones_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormTruck(), sender);
+        }
+
+        private void btnLotes_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormLot(), sender);
+        }
+
+        private void btnPaquetes_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormPackage(), sender);
+        }
+
+        private void btnTrayectos_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormJourney(), sender);
+        }
+    }
+}
